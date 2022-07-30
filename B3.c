5a57d4a05c07aa56024246e17b4cbd756428e24c
Connor-2022 <110145127+Connor-2022@users.noreply.github.com>
2022-07-30 11:26:32 +0800

    Add files via upload

---
+++ B3.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	printf("hello\n");
+
+		enum BLOOD
+		{
+	A,
+	B=5,
+	C,
+	};
+		enum BLOOD s = A;
+	printf("%d\n", A);
+	printf("%d\n", B);
+	printf("%d\n", C);
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int sum = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	sum = a + b + c;
+	printf("½á¹ûÎª%d\n", sum);
+	double height = 1.76;
+	int age = 18;
+	height = height + 0.3;
+		age = age + 2;
+		printf("height=%lf\n", height);
+		printf("age=%d\n", age);
+#define MAX 200
+		const MIN = 100;
+		printf("MAX=%d\n",MAX);
+		printf("MIN=%d\n", MIN);
+		char arr1[] = { "hehe" };
+		char arr2[] = { 'h','e' };
+		char arr3[] = { 'h','e','\0' };
+		printf("arr1=%d\n", strlen(arr1));
+		printf("arr2=%d\n", strlen(arr2));
+		printf("arr3=%d\n", strlen(arr3));
+		sum =so
+
+		return 0;
+
+}
