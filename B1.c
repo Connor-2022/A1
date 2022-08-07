23685c2a19db64ee054030a11afc1666aec8be7b
Connor-2022 <110145127+Connor-2022@users.noreply.github.com>
2022-08-07 19:29:15 +0800

    Add files via upload

---
+++ B1.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	printf("hello\n");
+//	printf("begin study\n");
+//	int num1 = 0;
+//	scanf("%d", &num1);
+//	if (num1 == 1)
+//		printf("huge salary\n");
+//	else
+//		printf("low salary\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d", &a, &b);
+//	c = a + b;
+//	printf("equal to%d\n", c);
+//	return 0;
+//}
+//int add(int a,int b)
+//{
+//	int c = a + b;
+//	return c;
+//	}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int num3 = add(num1, num2);
+//	printf("equal to%d", num3);
+//	return 0;
+//	}
+//int main()
+//{
+//	printf(" study basketball\n");
+//	int x = 0;
+//	while (x < 30)
+//	{
+//		printf("stduy time is%d\n", x);
+//		x++;
+//	}
+//	if (x == 30)
+//		printf("练习时长两年半\n");
+//	printf("全民制作人们大家好\n");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(double));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof("abc"));
+//	printf("%d\n", sizeof('a', 'b'));
+//	printf("%s\n", "hello world");
+//	printf("%c\n", 'a', 'b');
+//	printf("%c\n", '130');
+//	return 0;
+//}
